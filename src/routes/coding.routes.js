89db91aea41e9495c7280044cdc63b51a78d9c9b
Anshika Saxena <anshikasaxena314@gmail.com>
2026-05-28 12:50:58 +0530

    Removed duplicate root backend files

--- src/routes/coding.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getChallenges, getChallenge, submitSolution, seedChallenges } = require('../controllers/coding.controller');
-const { protect, authorize } = require('../middlewares/auth.middleware');
-router.get('/', protect, getChallenges);
-router.get('/:slug', protect, getChallenge);
-router.post('/:slug/submit', protect, submitSolution);
-router.post('/admin/seed', protect, authorize('admin'), seedChallenges);
-module.exports = router;
